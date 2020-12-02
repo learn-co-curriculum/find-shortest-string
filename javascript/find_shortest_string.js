4cca231ec84b25cc78d768f4ce59e3b605515b38
Rupa <rupa211984@gmail.com>
2020-12-02 12:30:59 -0800

    Add arrows

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -5,17 +5,17 @@ function findShortestString(arr) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log(findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log(findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log(findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
 }
--- ruby/find_shortest_string.rb
+++ ruby/find_shortest_string.rb
@@ -4,17 +4,17 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'a'"
-  puts find_shortest_string(['aaa', 'a', 'bb', 'ccc'])
+  puts "=>", find_shortest_string(['aaa', 'a', 'bb', 'ccc'])
 
   puts
 
   puts "Expecting: 'hi'"
-  puts find_shortest_string(['cat', 'hi', 'dog', 'an'])
+  puts "=>", find_shortest_string(['cat', 'hi', 'dog', 'an'])
 
   puts
 
   puts "Expecting: 'lily'"
-  puts find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
+  puts "=>", find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
 
   # Don't forget to add your own!
 
