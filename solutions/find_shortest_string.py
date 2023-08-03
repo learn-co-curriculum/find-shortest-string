ff5512f752eae5608d37493252f635ac9158e16f
Eleanor Kelman <ekelman3@gmail.com>
2023-08-03 15:15:16 -0400

    added Python solution

---
+++ solutions/find_shortest_string.py
@@ -0,0 +1,49 @@
+import time
+
+def find_shortest_string(list):
+
+    # THE LONGER WAY OF DOING THE SAME THING AS LINE 15
+
+    '''
+    shortest = list[0]
+
+    for item in list:
+        if len(item) < len(shortest):
+            shortest = item
+
+    return shortest
+    '''
+    
+    return min(list, key = len)
+
+if __name__ == '__main__':
+
+
+    print("Expecting: 'a'")
+    print(find_shortest_string(['aaa', 'a', 'bb', 'ccc']))
+
+    print("")
+
+    print("Expecting: 'hi'")
+    print(find_shortest_string(['cat', 'hi', 'dog', 'an']))
+
+    print("")
+
+    print("Expecting: 'lily'")
+    print(find_shortest_string(['flower', 'juniper', 'lily', 'dadelion']))
+
+    print("")
+
+    print("Expecting: 'cat'")
+    print(find_shortest_string(['cat']))
+
+    # BENCHMARK HERE
+
+    start_time = time.time()
+
+    for i in range(1000):
+        find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
+
+    avg_time = ( time.time() - start_time ) / 1000
+
+    print(avg_time)
